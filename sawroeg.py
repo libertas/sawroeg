e4cf937daaf49396a91da58d3c234ffea92bcf8f
Libertas <horizonvei@gmail.com>
2013-06-08 14:11:14 +0800

    move_to_center() completed

--- sawroeg.py
+++ sawroeg.py
@@ -10,15 +10,24 @@ import mainwindow
 
 class MainWindow(QtGui.QWidget,mainwindow.Ui_MainWindow):
 	def __init__(self,parent=None):
+		#get main window
 		QtGui.QWidget.__init__(self)
 		self.setupUi(self)
 		
+		self.move_to_center()
+		
+		#setup sawloih
 		self.searchingCuengh=False
 		self.bro_text=""
 		
 		for i in sawguq:
 			self.bro_text+=sawguq[i]
 		self.textBrowser.setText(self.bro_text)
+	
+	def move_to_center(self):
+		screen = QtGui.QDesktopWidget().screenGeometry()
+		size = self.geometry()
+		self.move((screen.width()-size.width())/2, (screen.height()-size.height())/2)
 	
 	def clearText(self):
 		self.textBrowser.setText(self.bro_text)
