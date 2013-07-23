8871aeb0cd9ece34bb0dd7acf47a49446fe9c712
Libertas <horizonvei@gmail.com>
2013-07-23 22:00:58 +0800

    new dictionary.py complete

--- dictionary.py
+++ dictionary.py
@@ -2,13 +2,27 @@
 from sawguq import sawguq
 from sawgeq import sawgeq
 
-def searchWord(key,method="HEAD"):#method can be "HEAD" or "MID"
-	if method=="HEAD":
+def searchWord(key,from_begin=True):
+	if from_begin:
 		if key in sawguq:
 			for i in sawguq[key]:
 				yield i
-	if method=="MID":
+	else:
 		for i in sawguq.values():
 			for j in i:
 				if key in j:
 					yield j
+
+def searchExamples(key):
+	result=""
+	for i in sawgeq:
+		string=sawgeq[i]
+		tlist=string.rsplit("\n")
+		for j in tlist:
+			if key in j:
+				result+="%s    --<<%s>>\n"%(j,i)
+				yield result
+
+if __name__=="__main__":
+	for i in searchExamples("goengbingz"):
+		print(i)
