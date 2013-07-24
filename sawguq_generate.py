3313c2aedd3949f72e40b9bf4a4345fd0c932053
Star Brilliant <m13253@hotmail.com>
2013-07-24 21:14:49 +0800

    Modify sawguq_generate.py
    File operations using with statements for security.

--- sawguq_generate.py
+++ sawguq_generate.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 
 sawguq = {}
-for word in open('sawguq.txt', 'r').read().splitlines():
-    word = word.replace('\u3000', '\x20').replace('(', '（').replace(
-        ')', '）').strip()
-    if not word:
-        continue
-    try:
-        idx, content = word.split(' ', 1)
-    except ValueError:
-        raise ValueError('Misformed line: %s' % repr(word))
-    idx = idx.strip('-').lower()
-    if idx in sawguq:
-        sawguq[idx].append(word)
-    else:
-        sawguq[idx] = [word]
-sawguq_py = open('sawguq.py', 'w')
-sawguq_py.write('sawguq = [\n')
-for word in sorted(sawguq):
-    sawguq[word].sort()
-    sawguq_py.write('    %s,\n' % repr((word, sawguq[word])))
-sawguq_py.write(']\n')
-sawguq_py.close()
+with open('sawguq.txt', 'r') as f:
+    for word in f.read().splitlines():
+        word = word.replace('\u3000', '\x20').replace('(', '（').replace(
+            ')', '）').strip()
+        if not word:
+            continue
+        try:
+            idx, content = word.split(' ', 1)
+        except ValueError:
+            raise ValueError('Misformed line: %s' % repr(word))
+        idx = idx.strip('-').lower()
+        if idx in sawguq:
+            sawguq[idx].append(word)
+        else:
+            sawguq[idx] = [word]
+with open('sawguq.py', 'w') as f:
+    f.write('sawguq = [\n')
+    for word in sorted(sawguq):
+        sawguq[word].sort()
+        f.write('    %s,\n' % repr((word, sawguq[word])))
+    f.write(']\n')
