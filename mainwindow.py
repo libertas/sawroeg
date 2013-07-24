42788dcb4b46dbf3b682dc9c1545ecf5640dfb54
Libertas <horizonvei@gmail.com>
2013-07-24 09:08:29 +0800

    change the width of comboBox in order to make the words show better

--- mainwindow.py
+++ mainwindow.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'mainwindow.ui'
 #
-# Created: Sun Jun 23 17:27:38 2013
-#      by: PyQt4 UI code generator 4.9.3
+# Created: Wed Jul 24 09:06:20 2013
+#      by: PyQt4 UI code generator 4.10.2
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -12,7 +12,16 @@ from PyQt4 import QtCore, QtGui
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
 except AttributeError:
-    _fromUtf8 = lambda s: s
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -21,7 +30,7 @@ class Ui_MainWindow(object):
         MainWindow.setMinimumSize(QtCore.QSize(0, 0))
         MainWindow.setMaximumSize(QtCore.QSize(494, 347))
         self.lineEdit = QtGui.QLineEdit(MainWindow)
-        self.lineEdit.setGeometry(QtCore.QRect(80, 10, 171, 41))
+        self.lineEdit.setGeometry(QtCore.QRect(90, 10, 161, 41))
         self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
         self.textBrowser = QtGui.QTextBrowser(MainWindow)
         self.textBrowser.setGeometry(QtCore.QRect(10, 60, 471, 281))
@@ -39,7 +48,7 @@ class Ui_MainWindow(object):
         self.pushButton_4.setGeometry(QtCore.QRect(320, 10, 51, 41))
         self.pushButton_4.setObjectName(_fromUtf8("pushButton_4"))
         self.comboBox = QtGui.QComboBox(MainWindow)
-        self.comboBox.setGeometry(QtCore.QRect(10, 10, 61, 41))
+        self.comboBox.setGeometry(QtCore.QRect(10, 10, 71, 41))
         self.comboBox.setObjectName(_fromUtf8("comboBox"))
 
         self.retranslateUi(MainWindow)
@@ -51,9 +60,9 @@ class Ui_MainWindow(object):
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "Saw Roeg", None, QtGui.QApplication.UnicodeUTF8))
-        self.pushButton.setText(QtGui.QApplication.translate("MainWindow", "&Ra", None, QtGui.QApplication.UnicodeUTF8))
-        self.pushButton_2.setText(QtGui.QApplication.translate("MainWindow", "?", None, QtGui.QApplication.UnicodeUTF8))
-        self.pushButton_3.setText(QtGui.QApplication.translate("MainWindow", "N&daep", None, QtGui.QApplication.UnicodeUTF8))
-        self.pushButton_4.setText(QtGui.QApplication.translate("MainWindow", "&Baet", None, QtGui.QApplication.UnicodeUTF8))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Saw Roeg", None))
+        self.pushButton.setText(_translate("MainWindow", "&Ra", None))
+        self.pushButton_2.setText(_translate("MainWindow", "?", None))
+        self.pushButton_3.setText(_translate("MainWindow", "N&daep", None))
+        self.pushButton_4.setText(_translate("MainWindow", "&Baet", None))
 
