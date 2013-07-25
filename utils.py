6038fa13d85af8950e7247b4d9587ea1771a8b73
Star Brilliant <m13253@hotmail.com>
2013-07-25 11:17:11 +0800

    Rewrite utils.iter_slice

--- utils.py
+++ utils.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 
 def iter_slice(iter, start=0, end=None):
-    try:
-        for i in range(start):
-            next(iter)
-        for i in range(start, end):
-            yield next(iter)
-    except StopIteration:
-        raise IndexError('iter_slice(iter, start=%s, end=%s) out of range' %
-            (repr(start), repr(end)))
+    for i in range(start):
+        next(iter)
+    for i in range(start, end):
+        yield (next(iter),)
+    next(iter)
+    yield None
