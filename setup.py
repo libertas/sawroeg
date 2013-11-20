788832d4cdee94dfca281987d73c6c22a5010882
libertas <horizonvei@gmail.com>
2013-11-20 17:30:30 +0800

    can use options in setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,37 @@
 
 import os
 import sys
+import getopt
+
+def print_help():
+    print("""Usage:%s [-h|-H] [--help|--prefix|--pyname]"""%Dsys.argv[0])
+
 if os.name == "posix":
-    python_name = "python3"  # change it if it's not "python3"
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hH", ["help", "prefix=","pyname="])
+    except getopt.GetoptError:
+        pass
+    
+    for opt,result in opts:
+        if opt in ("-h","H","--help"):
+            print_help()
+        
+        if opt in ("--prefix="):
+            prefix=result
+        else:
+            prefix="/usr/local/"
+        
+        if opt in ("--pyname"):
+            python_name=result
+        else:
+            python_name = "python3"
+    
     os.system("mkdir -p /opt/sawroeg/")
     os.system("cp -r ./ /opt/sawroeg/")
-    os.system("ln /opt/sawroeg/sawroeg.desktop /usr/local/share/applications/sawroeg.desktop")
-    os.system('echo "cd /opt/sawroeg/\n'+python_name+' /opt/sawroeg/sawroeg.py" > /usr/local/bin/sawroeg')
+    os.system("ln /opt/sawroeg/sawroeg.desktop %s/share/applications/sawroeg.desktop"%prefix)
+    os.system('echo "cd /opt/sawroeg/\n'+python_name+' /opt/sawroeg/sawroeg.py" > %s/bin/sawroeg'%prefix)
     os.system("chmod +rx /opt/sawroeg")
-    os.system("chmod +x /usr/local/bin/sawroeg")
+    os.system("chmod +x %s/bin/sawroeg"%prefix)
 elif os.name == "nt":
     if len(sys.argv) == 1:
         os.system("setup.py build")
