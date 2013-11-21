46a1e56f17bc9ba1cd94981f4877e1107694347d
libertas <horizonvei@gmail.com>
2013-11-21 10:25:28 +0800

    remove unused file

--- utils.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-def iter_slice(iter, start=0, end=None):
-    for i in range(start):
-        next(iter)
-    for i in range(start, end):
-        yield (next(iter),)
-    next(iter)
-    yield None
