3105d241c3e8108177f81f39526a60f8f3e83faf
Libertas <horizonvei@gmail.com>
2014-02-03 18:26:31 +0800

    checkbox label

--- mainwindow.py
+++ mainwindow.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'mainwindow.ui'
 #
-# Created: Thu Jan 30 16:27:18 2014
+# Created: Mon Feb  3 18:25:04 2014
 #      by: PyQt4 UI code generator 4.9.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -21,10 +21,10 @@ class Ui_MainWindow(object):
         MainWindow.setMinimumSize(QtCore.QSize(0, 0))
         MainWindow.setMaximumSize(QtCore.QSize(494, 347))
         self.lineEdit = QtGui.QLineEdit(MainWindow)
-        self.lineEdit.setGeometry(QtCore.QRect(90, 10, 151, 41))
+        self.lineEdit.setGeometry(QtCore.QRect(80, 10, 151, 41))
         self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
         self.textBrowser = QtGui.QTextBrowser(MainWindow)
-        self.textBrowser.setGeometry(QtCore.QRect(10, 60, 471, 281))
+        self.textBrowser.setGeometry(QtCore.QRect(10, 50, 471, 281))
         self.textBrowser.setObjectName(_fromUtf8("textBrowser"))
         self.pushButton = QtGui.QPushButton(MainWindow)
         self.pushButton.setGeometry(QtCore.QRect(260, 10, 51, 41))
@@ -33,17 +33,20 @@ class Ui_MainWindow(object):
         self.pushButton_2.setGeometry(QtCore.QRect(380, 20, 31, 28))
         self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
         self.pushButton_3 = QtGui.QPushButton(MainWindow)
-        self.pushButton_3.setGeometry(QtCore.QRect(420, 10, 61, 41))
+        self.pushButton_3.setGeometry(QtCore.QRect(420, 10, 51, 41))
         self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
         self.pushButton_4 = QtGui.QPushButton(MainWindow)
         self.pushButton_4.setGeometry(QtCore.QRect(320, 10, 51, 41))
         self.pushButton_4.setObjectName(_fromUtf8("pushButton_4"))
         self.comboBox = QtGui.QComboBox(MainWindow)
-        self.comboBox.setGeometry(QtCore.QRect(10, 10, 71, 41))
+        self.comboBox.setGeometry(QtCore.QRect(10, 10, 61, 41))
         self.comboBox.setObjectName(_fromUtf8("comboBox"))
         self.levenshtein = QtGui.QCheckBox(MainWindow)
-        self.levenshtein.setGeometry(QtCore.QRect(240, 30, 21, 20))
+        self.levenshtein.setGeometry(QtCore.QRect(230, 30, 21, 16))
         self.levenshtein.setObjectName(_fromUtf8("levenshtein"))
+        self.label = QtGui.QLabel(MainWindow)
+        self.label.setGeometry(QtCore.QRect(230, 10, 21, 16))
+        self.label.setObjectName(_fromUtf8("label"))
 
         self.retranslateUi(MainWindow)
         QtCore.QObject.connect(self.lineEdit, QtCore.SIGNAL(_fromUtf8("returnPressed()")), MainWindow.newSearch)
@@ -59,5 +62,6 @@ class Ui_MainWindow(object):
         self.pushButton_2.setText(QtGui.QApplication.translate("MainWindow", "?", None, QtGui.QApplication.UnicodeUTF8))
         self.pushButton_3.setText(QtGui.QApplication.translate("MainWindow", "N&daep", None, QtGui.QApplication.UnicodeUTF8))
         self.pushButton_4.setText(QtGui.QApplication.translate("MainWindow", "&Baet", None, QtGui.QApplication.UnicodeUTF8))
-        self.levenshtein.setText(QtGui.QApplication.translate("MainWindow", "CheckBox", None, QtGui.QApplication.UnicodeUTF8))
+        self.levenshtein.setText(QtGui.QApplication.translate("MainWindow", "Cinj", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("MainWindow", "Cinj", None, QtGui.QApplication.UnicodeUTF8))
 
