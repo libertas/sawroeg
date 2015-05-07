15b27d3f36e734ccf4ee14e1905ddec10889bdac
Libertas <horizonvei@gmail.com>
2015-05-07 22:41:00 +0800

    remove old api

--- sawroeg_web.py
+++ sawroeg_web.py
@@ -177,59 +177,6 @@ class ComposeHandler(tornado.web.RequestHandler):
         self.render("sawroeg-compose.html",  success=True)
 
 
-class ApiHandler(tornado.web.RequestHandler):
-    def get(self):
-        # I think it is better to copy it twice rather than make it in a func.
-        start = 0
-        try:
-            start = max(int(self.get_argument("start")), 0)
-        except (ValueError, tornado.web.MissingArgumentError):
-            pass
-        count = None
-        try:
-            count = max(int(self.get_argument("count")), 1)
-        except (ValueError, tornado.web.MissingArgumentError):
-            pass
-        api_type = 'xml'
-        try:
-            api_type = self.get_argument("type")
-        except tornado.web.MissingArgumentError:
-            pass
-        if api_type not in ('xml', 'json'):
-            self.set_status(400)
-            self.set_header("Content-Type", "text/plain; charset=utf-8")
-            self.render("sawroeg-api-help.html")
-            return
-        try:
-            key = self.get_argument("q")
-        except tornado.web.MissingArgumentError:
-            self.set_status(400)
-            self.set_header("Content-Type", "text/plain; charset=utf-8")
-            self.render("sawroeg-api-help.html")
-            return
-        result = list(dictionary.searchWord(key)) if key else []
-        if count is None:
-            have_more = False
-            del result[:start]
-        else:
-            have_more = len(result) > start+count
-            result = result[start:start + count]
-        if api_type == 'xml':
-            self.set_header("Content-Type", "text/xml; charset=utf-8")
-            template_args = {
-                "key": key, "start": start, "count": count,
-                "result": result, "have_more": have_more
-            }
-            self.render("sawroeg-api-xml.html", **template_args)
-        elif api_type == 'json':
-            self.set_header("Content-Type", "text/json; charset=utf-8")
-            if have_more:
-                result.append(('...', []))
-            self.write(json.dumps(result))
-        else:
-            raise RuntimeError('program should not reach here')
-
-
 if __name__ == "__main__":
     tornado.options.define(
         "debug", default=False,
@@ -257,7 +204,6 @@ if __name__ == "__main__":
     }
     application = tornado.web.Application([
         ("/sawroeg", SearchHandler),
-        ("/api", ApiHandler),
         ("/download",  DownloadHandler),
         ("/download/(.*)",  DownloadHandler),
          ("/login",  LoginHandler),
