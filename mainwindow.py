b2bf6c461528429a089c17f197ca53dffbafb716
libertas <horizonvei@gmail.com>
2019-05-26 21:54:30 +0800

    enlarge the ui to display more

--- mainwindow.py
+++ mainwindow.py
@@ -1,39 +1,40 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'mainwindow.ui'
+# Form implementation generated from reading ui file '.\mainwindow.ui'
 #
-# Created by: PyQt5 UI code generator 5.7
+# Created by: PyQt5 UI code generator 5.12.2
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(494, 347)
+        MainWindow.resize(720, 480)
         MainWindow.setMinimumSize(QtCore.QSize(0, 0))
-        MainWindow.setMaximumSize(QtCore.QSize(494, 347))
+        MainWindow.setMaximumSize(QtCore.QSize(720, 480))
         self.lineEdit = QtWidgets.QLineEdit(MainWindow)
-        self.lineEdit.setGeometry(QtCore.QRect(80, 10, 171, 41))
+        self.lineEdit.setGeometry(QtCore.QRect(170, 20, 281, 41))
         self.lineEdit.setObjectName("lineEdit")
         self.textBrowser = QtWidgets.QTextBrowser(MainWindow)
-        self.textBrowser.setGeometry(QtCore.QRect(10, 70, 471, 271))
+        self.textBrowser.setGeometry(QtCore.QRect(10, 80, 701, 381))
         self.textBrowser.setObjectName("textBrowser")
         self.pushButton = QtWidgets.QPushButton(MainWindow)
-        self.pushButton.setGeometry(QtCore.QRect(260, 10, 51, 41))
+        self.pushButton.setGeometry(QtCore.QRect(460, 20, 51, 41))
         self.pushButton.setObjectName("pushButton")
         self.pushButton_2 = QtWidgets.QPushButton(MainWindow)
-        self.pushButton_2.setGeometry(QtCore.QRect(380, 17, 31, 21))
+        self.pushButton_2.setGeometry(QtCore.QRect(590, 20, 31, 41))
         self.pushButton_2.setObjectName("pushButton_2")
         self.pushButton_3 = QtWidgets.QPushButton(MainWindow)
-        self.pushButton_3.setGeometry(QtCore.QRect(420, 10, 71, 41))
+        self.pushButton_3.setGeometry(QtCore.QRect(630, 20, 71, 41))
         self.pushButton_3.setObjectName("pushButton_3")
         self.pushButton_4 = QtWidgets.QPushButton(MainWindow)
-        self.pushButton_4.setGeometry(QtCore.QRect(310, 10, 61, 41))
+        self.pushButton_4.setGeometry(QtCore.QRect(520, 20, 61, 41))
         self.pushButton_4.setObjectName("pushButton_4")
         self.comboBox = QtWidgets.QComboBox(MainWindow)
-        self.comboBox.setGeometry(QtCore.QRect(10, 10, 61, 41))
+        self.comboBox.setGeometry(QtCore.QRect(10, 20, 151, 41))
         self.comboBox.setObjectName("comboBox")
 
         self.retranslateUi(MainWindow)
@@ -51,4 +52,5 @@ class Ui_MainWindow(object):
         self.pushButton_2.setText(_translate("MainWindow", "?"))
         self.pushButton_3.setText(_translate("MainWindow", "N&daep"))
         self.pushButton_4.setText(_translate("MainWindow", "&Baet"))
+
 
