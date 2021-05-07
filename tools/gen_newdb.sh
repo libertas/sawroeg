c030318f1a7ff6890880ce6052cd67acb0bfd47b
libertas <horizonvei@gmail.com>
2021-05-07 13:02:08 +0800

    add gen_newdb.sh

---
+++ tools/gen_newdb.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+python3 sawguq_generate.py -f sawcaux.txt newdict.db
+python3 sawguq_generate.py -f sawyimmoq1.txt newdict.db
+python3 sawguq_generate.py -f gvaefamh_1.txt newdict.db
