9cadc401c3a68a982d5e96450a3182c8fe261baf
libertas <horizonvei@gmail.com>
2021-05-07 12:40:52 +0800

    add gen_db.sh

---
+++ tools/gen_db.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+rm -f sawguq.db
+python3 sawguq_generate.py -f sawcaux.txt sawguq.db
+python3 sawguq_generate.py -f sawyimmoq1.txt sawguq.db
+python3 sawguq_generate.py -f gvaefamh_1.txt sawguq.db
+python3 sawguq_generate.py -f sawguq.txt sawguq.db
